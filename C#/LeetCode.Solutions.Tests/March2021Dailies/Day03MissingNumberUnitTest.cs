49859173d60c3cbe9883e90cfde57347bd650971
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-03-03 20:53:53 +0100

    Leetcode march 2021 - Day 3

---
+++ C#/LeetCode.Solutions.Tests/March2021Dailies/Day03MissingNumberUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.March2021Dailies;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace LeetCode.Solutions.Tests.March2021Dailies
+{
+    [TestFixture]
+    public class Day03MissingNumberUnitTest
+    {
+        private Day03MissingNumber _sut;
+        private Stopwatch _stopwatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Day03MissingNumber();
+            _stopwatch = new Stopwatch();
+        }
+
+        [TestCase(new[] { 3, 0, 1 }, 2)]
+        [TestCase(new[] { 0, 1 }, 2)]
+        [TestCase(new[] { 9, 6, 4, 2, 3, 5, 7, 0, 1 }, 8)]
+        [TestCase(new[] { 0 }, 1)]
+        [TestCase(new[] { 1 }, 0)]
+        public void MissingNumber_GivenExample_ShouldMatchExpected(int[] nums, int expected)
+        {
+            // Given When
+            _stopwatch.Start();
+            var result = _sut.MissingNumber(nums);
+            _stopwatch.Stop();
+
+            // Then
+            result.Should().Be(expected);
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Milliseconds} ms");
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Ticks} ticks");
+        }
+    }
+}
---
+++ C#/LeetCode.Solutions/March2021Dailies/Day03MissingNumber.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace LeetCode.Solutions.March2021Dailies
+{
+    // https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3659/
+    public class Day03MissingNumber
+    {
+        public int MissingNumber(int[] nums)
+        {
+            return Enumerable.Range(0, nums.Length + 1).Except(nums).First();
+        }
+    }
+}
