796cd81209ec01fc61f022cdb9c13f14eff2548a
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-03-03 22:02:30 +0100

    Leetcode march 2021 - Day 2

---
+++ C#/LeetCode.Solutions.Tests/March2021Dailies/Day02SetMismatchUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.March2021Dailies;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace LeetCode.Solutions.Tests.March2021Dailies
+{
+    [TestFixture]
+    public class Day02SetMismatchUnitTest
+    {
+        private Day02SetMismatch _sut;
+        private Stopwatch _stopwatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Day02SetMismatch();
+            _stopwatch = new Stopwatch();
+        }
+
+        [TestCase(new[] { 1, 2, 2, 4 }, new[] { 2, 3 })]
+        [TestCase(new[] { 1, 2, 3, 2 }, new[] { 2, 4 })]
+        [TestCase(new[] { 1, 1 }, new[] { 1, 2 })]
+        public void FindErrorNums_GivenExample_ShouldMatchExpected(int[] nums, int[] expected)
+        {
+            // Given When
+            _stopwatch.Start();
+            var result = _sut.FindErrorNums(nums);
+            _stopwatch.Stop();
+
+            // Then
+            result.Should().BeEquivalentTo(expected);
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Milliseconds} ms");
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Ticks} ticks");
+        }
+    }
+}
---
+++ C#/LeetCode.Solutions/March2021Dailies/Day02SetMismatch.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace LeetCode.Solutions.March2021Dailies
+{
+    // https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/588/week-1-march-1st-march-7th/3658/
+    public class Day02SetMismatch
+    {
+        public int[] FindErrorNums(int[] nums)
+        {
+            var missing = Enumerable.Range(1, nums.Length).Except(nums).First();
+            var duplicate = nums.GroupBy(n => n).Where(g => g.Count() > 1).Select(g => g.Key).First();
+            return new[] { duplicate, missing };
+        }
+    }
+}
