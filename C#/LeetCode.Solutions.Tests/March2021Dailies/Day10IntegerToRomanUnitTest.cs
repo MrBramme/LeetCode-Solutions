a5d6b65fdfed742b9cbd5631887d4d27d29e68d3
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-03-10 21:24:37 +0100

    Leetcode march 2021 - Day 10

---
+++ C#/LeetCode.Solutions.Tests/March2021Dailies/Day10IntegerToRomanUnitTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.March2021Dailies;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace LeetCode.Solutions.Tests.March2021Dailies
+{
+    [TestFixture]
+    public class Day10IntegerToRomanUnitTest
+    {
+        private Day10IntegerToRoman _sut;
+        private Stopwatch _stopwatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Day10IntegerToRoman();
+            _stopwatch = new Stopwatch();
+        }
+
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        [TestCase(5, "V")]
+        [TestCase(6, "VI")]
+        [TestCase(7, "VII")]
+        [TestCase(8, "VIII")]
+        [TestCase(9, "IX")]
+        [TestCase(10, "X")]
+        [TestCase(11, "XI")]
+        [TestCase(12, "XII")]
+        [TestCase(49, "XLIX")]
+        [TestCase(50, "L")]
+        [TestCase(58, "LVIII")]
+        [TestCase(99, "XCIX")]
+        [TestCase(100, "C")]
+        [TestCase(101, "CI")]
+        [TestCase(499, "CDXCIX")]
+        [TestCase(500, "D")]
+        [TestCase(501, "DI")]
+        [TestCase(999, "CMXCIX")]
+        [TestCase(1000, "M")]
+        [TestCase(1001, "MI")]
+        [TestCase(1994, "MCMXCIV")]
+        public void IntToRoman_GivenInput_ReturnsExpectedRoman(int num, string expected)
+        {
+            // Given When
+            var roman = _sut.IntToRoman(num);
+
+            // Then
+            roman.Should().Be(expected);
+        }
+    }
+}
---
+++ C#/LeetCode.Solutions/March2021Dailies/Day10IntegerToRoman.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions.March2021Dailies
+{
+    // https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3667/
+    public class Day10IntegerToRoman
+    {
+        private static readonly Dictionary<string, int> _romanValues = new Dictionary<string, int>
+        {
+            {"M", 1000},
+            {"D", 500},
+            {"C", 100},
+            {"L", 50},
+            {"X", 10},
+            {"V", 5},
+            {"I", 1},
+        };
+
+        private static readonly Dictionary<string, string> _romanPrefixes = new Dictionary<string, string>
+        {
+            {"M", "C"},
+            {"D", "C"},
+            {"C", "X"},
+            {"L", "X"},
+            {"X", "I"},
+            {"V", "I"},
+            {"I", "I"},
+        };
+
+        public string IntToRoman(int num)
+        {
+            var sb = new StringBuilder();
+
+            num = ProcessRoman(num, sb, "M");
+            num = ProcessRoman(num, sb, "D");
+            num = ProcessRoman(num, sb, "C");
+            num = ProcessRoman(num, sb, "L");
+            num = ProcessRoman(num, sb, "X");
+            num = ProcessRoman(num, sb, "V");
+            num = ProcessRoman(num, sb, "I");
+
+            return sb.ToString();
+        }
+
+        private static int ProcessRoman(int num, StringBuilder sb, string roman)
+        {
+            while (num >= _romanValues[roman])
+            {
+                sb.Append(roman);
+                num -= _romanValues[roman];
+            }
+
+            if (num > 0 && num >= _romanValues[roman] - _romanValues[_romanPrefixes[roman]])
+            {
+                sb.Append(_romanPrefixes[roman]);
+                sb.Append(roman);
+                num -= _romanValues[roman] - _romanValues[_romanPrefixes[roman]];
+            }
+
+            return num;
+        }
+    }
+}
