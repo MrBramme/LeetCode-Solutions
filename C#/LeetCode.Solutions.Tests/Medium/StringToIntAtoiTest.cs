03054b23176465c05c7f029c9336e917add1ac61
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-10-27 22:11:11 +0200

    Added string to int Atoi

---
+++ C#/LeetCode.Solutions.Tests/Medium/StringToIntAtoiTest.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.Medium;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace LeetCode.Solutions.Tests.Medium
+{
+    public class StringToIntAtoiTest
+    {
+        private StringToIntAtoi _sut;
+        private Stopwatch _stopwatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new StringToIntAtoi();
+            _stopwatch = new Stopwatch();
+        }
+
+        [TestCase("42", 42)]
+        [TestCase("   -42", -42)]
+        [TestCase("4193 with words", 4193)]
+        [TestCase("words and 987", 0)]
+        [TestCase("-91283472332", -2147483648)]
+        [TestCase("", 0)]
+        [TestCase("   +42", 42)]
+        public void Examples(string input, int expected)
+        {
+            // Given
+
+            // When
+            _stopwatch.Start();
+            var result = _sut.MyAtoi(input);
+            _stopwatch.Stop();
+
+            // Then
+            result.Should().Be(expected);
+
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Milliseconds} ms");
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Ticks} ticks");
+        }
+    }
+}
---
+++ C#/LeetCode.Solutions/Medium/StringToIntAtoi.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode.Solutions.Medium
+{
+    // https://leetcode.com/problems/string-to-integer-atoi/
+    public class StringToIntAtoi
+    {
+        // Procedural approach alert!!! :D
+        public int MyAtoi(string s)
+        {
+            var charArray = s.Trim().ToCharArray();
+            if (charArray.Length == 0)
+            {
+                return 0;
+            }
+
+            var isNeg = charArray[0] == '-';
+            var startPos = isNeg || charArray[0] == '+' ? 1 : 0;
+            var currentPos = startPos;
+
+            var isDigit = true;
+            while (currentPos < charArray.Length && isDigit)
+            {
+                isDigit = char.IsDigit(charArray[currentPos]);
+                if (isDigit)
+                {
+                    currentPos++;
+                }
+            }
+
+            if (startPos == currentPos)
+            {
+                return 0;
+            }
+
+            var numStr = string.Format("{0}{1}", isNeg ? "-" : "+", new string(charArray, startPos, currentPos - startPos));
+            if (!int.TryParse(numStr, out var res))
+            {
+                res = isNeg ? int.MinValue : int.MaxValue;
+            }
+
+            return res;
+        }
+    }
+}
