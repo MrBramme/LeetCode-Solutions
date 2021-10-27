453df9b17f695f0a3f34b71dc3d9c2360ac6bd45
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-10-27 20:53:36 +0200

    half commit of changes i didn't get to

---
+++ C#/LeetCode.Solutions.Tests/March2021Dailies/Day12BinaryCodesUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.March2021Dailies;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace LeetCode.Solutions.Tests.March2021Dailies
+{
+    [TestFixture]
+    public class Day12BinaryCodesUnitTest
+    {
+        private Day12BinaryCodes _sut;
+        private Stopwatch _stopwatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new Day12BinaryCodes();
+            _stopwatch = new Stopwatch();
+        }
+
+        [TestCase("00110110", 2, true)]
+        [TestCase("00110", 2, true)]
+        [TestCase("0110", 1, true)]
+        [TestCase("0110", 2, false)]
+        [TestCase("0000000001011100", 4, false)]
+        public void IntToRoman_GivenInput_ReturnsExpectedRoman(string s, int k, bool expected)
+        {
+            // Given When
+            var roman = _sut.HasAllCodes(s, k);
+
+            // Then
+            roman.Should().Be(expected);
+        }
+    }
+}
---
+++ C#/LeetCode.Solutions/March2021Dailies/Day12BinaryCodes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode.Solutions.March2021Dailies
+{
+    // https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3669/
+    public class Day12BinaryCodes
+    {
+        public bool HasAllCodes(string s, int k)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
