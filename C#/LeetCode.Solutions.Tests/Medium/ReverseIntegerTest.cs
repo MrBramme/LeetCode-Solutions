dce9a172a2d62883a60016aaff3963b2a6307eb1
Bram Hendrickx <bram_hendrickx@hotmail.com>
2021-10-27 21:38:48 +0200

    int reversing

---
+++ C#/LeetCode.Solutions.Tests/Medium/ReverseIntegerTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.Medium;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace LeetCode.Solutions.Tests.Medium
+{
+    public class ReverseIntegerTest
+    {
+        private ReverseInteger _sut;
+        private Stopwatch _stopwatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new ReverseInteger();
+            _stopwatch = new Stopwatch();
+        }
+
+        [TestCase(123, 321)]
+        [TestCase(-123, -321)]
+        [TestCase(120, 21)]
+        [TestCase(0, 0)]
+        public void Examples(int input, int expected)
+        {
+            // Given
+
+            // When
+            _stopwatch.Start();
+            var result = _sut.Reverse(input);
+            _stopwatch.Stop();
+
+            // Then
+            result.Should().Be(expected);
+
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Milliseconds} ms");
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Ticks} ticks");
+        }
+    }
+}
---
+++ C#/LeetCode.Solutions/Medium/ReverseInteger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LeetCode.Solutions.Medium
+{
+    // https://leetcode.com/problems/reverse-integer/
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            var charArray = x.ToString().Replace("-", "").ToCharArray();
+            Array.Reverse(charArray, 0, charArray.Length);
+            var res = 0;
+            int.TryParse(new string(charArray), out res);
+            return x >= 0 ? res : res * -1;
+        }
+    }
+}
