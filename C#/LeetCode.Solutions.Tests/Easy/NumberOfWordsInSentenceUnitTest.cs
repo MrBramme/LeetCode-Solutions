82edf1d645f4a8c588a27441606084784516ec07
Bram Hendrickx <bram_hendrickx@hotmail.com>
2022-01-19 21:05:18 +0100

    Easy: number of words in sentences

---
+++ C#/LeetCode.Solutions.Tests/Easy/NumberOfWordsInSentenceUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using LeetCode.Solutions.Easy;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace LeetCode.Solutions.Tests.Easy
+{
+    public class NumberOfWordsInSentenceUnitTest
+    {
+        private NumberOfWordsInSentence _sut;
+        private Stopwatch _stopwatch;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new NumberOfWordsInSentence();
+            _stopwatch = new Stopwatch();
+        }
+
+        [TestCase(new[] { "alice and bob love leetcode", "i think so too", "this is great thanks very much"}, 6)]
+        [TestCase(new[] { "please wait", "continue to fight", "continue to win" }, 3)]
+        public void KidsWithTheGreatestNumberOfCandies_GivenExample_ShouldReturnTarget(string[] input, int expected)
+        {
+            // Given
+
+            // When
+            _stopwatch.Start();
+            var result = _sut.MostWordsFound(input);
+            _stopwatch.Stop();
+
+            // Then
+            result.Should().Be(expected);
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Milliseconds} ms");
+            Console.WriteLine($"Ran for: {_stopwatch.Elapsed.Ticks} ticks");
+        }
+    }
+}
---
+++ C#/LeetCode.Solutions/Easy/NumberOfWordsInSentence.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Solutions.Easy
+{
+    // https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+    public class NumberOfWordsInSentence
+    {
+        public int MostWordsFound(string[] sentences)
+        {
+            return sentences.Select(x => x.ToCharArray().Count(c => c.Equals(' ')) + 1).Max();
+        }
+    }
+}
